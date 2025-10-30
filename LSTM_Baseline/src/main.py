e85edab0c8fdfc62e6d6756973eb490233882a40
cynnaaamon <cynthiasnijder@gmail.com>
2025-10-30 14:16:00 +0100

    implemented DBSCAN, still need to do the plotting

--- LSTM_Baseline/src/main.py
+++ LSTM_Baseline/src/main.py
@@ -1,7 +1,7 @@
 # 1: pre-process data
+testing:
 # 2: train LSTM on data
 # 3: run LSTM
-
 
 # need functions for:
 #   - train model
@@ -15,7 +15,16 @@
 #   - Avg. MAPE Aggregating forecast
 #   - Avg. MAPE forecasting the aggregate
 
+# Pre processing:
+#   1. isolate subset of used data in Kong et al.   [done]
+#   2. Run DBSCAN on the data                       [todo] 
+#   3. Encode data for the model                    [todo]
 
+# Model:
+#   1. train model                                  [todo]
+#   2. load/save model                              [todo]
 
-# Let's start with preparing the data:
-#   - DBSCAN is used for clustering sensible (?) data, need to understand why
+# Metrics:
+#   1. Avg. MAPE individual forecast                [todo]
+#   2. Avg. MAPE Aggregating forecast               [todo]
+#   3. Avg. MAPE forecasting the aggregate          [todo]
